b7a0f9a6e7b9c48319c62584d369dd95fea9d69c
rodrigo052003 <rodrigozinho06@gmail.com>
2025-07-24 01:28:58 -0300

    Sua mensagem explicando a mudança

--- src/janelaprincipal/JogoTeste.java
+++ src/janelaprincipal/JogoTeste.java
@@ -1,8 +1,8 @@
-package gui;
+package janelaprincipal;
 
 import apresentacao.TelaQuiz;
 
-public class Main {
+public class JogoTeste {
     public static void main(String[] args) {
         new TelaQuiz();
     }
